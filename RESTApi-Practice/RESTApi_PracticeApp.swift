3c521130e9ab375e059aff57249e8314bfd77129
Amish <amish.tufail2002@gmail.com>
2024-07-26 21:45:17 +0500

    Initial Commit

---
+++ RESTApi-Practice/RESTApi_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RESTApi_PracticeApp.swift
+//  RESTApi-Practice
+//
+//  Created by Amish on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RESTApi_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
